5e95e224d08bed9d5964a149889920ac37e26ec4
BassManLuke <luke_haz@yahoo.co.uk>
2024-09-13 21:16:40 +0100

    update seed file

---
+++ db/data/test-data/index.js
@@ -0,0 +1,4 @@
+exports.typeData = require('./types.js');
+exports.userData = require('./users.js');
+exports.plantData = require('./plants.js');
+exports.plantsFoundData = require('./plants_found.js');
--- db/seeds/seed.js
+++ db/seeds/seed.js
@@ -1,6 +1,7 @@
+const format = require('pg-format')
 const db = require("../connection")
 
-function seed ({}) {
+function seed ({typeData}) {
     return db.query(`DROP TABLE IF EXISTS plants-found;`)
     .then(() => {
         return db.query(`DROP TABLE IF EXISTS plants;`)
@@ -11,6 +12,41 @@ function seed ({}) {
     .then(() => {
         return db.query(`DROP TABLE IF EXISTS types;`)
     })
+    .then(() => {
+        return db.query(
+            `CREATE TABLE types (
+            type_id SERIAL PRIMARY KEY,
+            plant_name VARCHAR(30) UNIQUE NOT NULL
+            about_plant VARCHARD NOT NULL
+            );`
+        )
+    })
+    .then(() => {
+        const insertTypesData = format(
+            `INSERT INTO types (
+            plant_name, about_plant
+            ) VALUES %L;`,
+            typeData.map(({plant_name, about_plant }) => {
+                [plant_name, about_plant]
+            })
+        )
+        return db.query(insertTypesData)
+        .then(() => {
+            return db.query(`SELECT * FROM types;`)
+            .then((result) => {
+                console.log(result)
+            })
+        })
+    })
+
+
+
+
+
+
+
+
+
 }
 
 module.exports = seed
