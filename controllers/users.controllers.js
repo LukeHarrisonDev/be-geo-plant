eb8feacf27bdd612f32b6b1f8e9e43ca1b184f7e
BassManLuke <luke_haz@yahoo.co.uk>
2024-09-25 17:16:35 +0100

    remove console logs

--- controllers/users.controllers.js
+++ controllers/users.controllers.js
@@ -3,7 +3,6 @@ const { fetchUsers } = require("../models/users.models")
 function getUsers(request, response, next) {
     fetchUsers()
     .then((users) => {
-        // console.log(users, "<<< Users")
         response.status(200).send({users})
     })
     
