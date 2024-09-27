5724589c847639ee81899c0ea9d6f30e14d6b0c7
BassManLuke <luke_haz@yahoo.co.uk>
2024-09-27 11:56:00 +0100

    refactor routes

--- app.js
+++ app.js
@@ -1,17 +1,10 @@
 const express = require("express")
 const apiRouter = require("./routes/api-router")
-const { getUsers } = require("./controllers/users.controllers")
-const { getPlants } = require("./controllers/plants.controllers")
-const { getFoundPlants } = require("./controllers/found-plants.controller")
 const { catchInvalidEndpoints } = require("./error-handling")
 const app = express()
 const endpoints = require("./endpoints.json")
 
 app.use("/api", apiRouter)
-
-app.get("/api/users", getUsers)
-app.get("/api/plants", getPlants)
-app.get("/api/found_plants", getFoundPlants)
 
 app.all("/*", catchInvalidEndpoints)
 
