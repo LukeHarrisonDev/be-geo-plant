f3b5fbedffac426832c1ff67be51ae1f0527d33a
BassManLuke <luke_haz@yahoo.co.uk>
2024-09-27 11:56:00 +0100

    refactor routes

--- routes/api-router.js
+++ routes/api-router.js
@@ -1,9 +1,15 @@
 const apiRouter = require("express").Router()
 const endpoints = require("../endpoints.json")
+const { getUsers } = require("../controllers/users.controllers")
+const { getPlants } = require("../controllers/plants.controllers")
+const { getFoundPlants } = require("../controllers/found-plants.controller")
 
 apiRouter.get("/", (request, response) => {
     response.status(200).send({ endpoints })
 })
 
+apiRouter.get("/users", getUsers)
+apiRouter.get("/plants", getPlants)
+apiRouter.get("/found_plants", getFoundPlants)
 
 module.exports = apiRouter
