638f7d48096a9727ed0bf18cc865dfcbb41997ff
BassManLuke <luke_haz@yahoo.co.uk>
2024-09-27 12:00:55 +0100

    add usersrouter

--- routes/api-router.js
+++ routes/api-router.js
@@ -1,14 +1,15 @@
 const apiRouter = require("express").Router()
 const endpoints = require("../endpoints.json")
-const { getUsers } = require("../controllers/users.controllers")
 const { getPlants } = require("../controllers/plants.controllers")
 const { getFoundPlants } = require("../controllers/found-plants.controller")
+const usersRouter = require("./users-router")
 
 apiRouter.get("/", (request, response) => {
     response.status(200).send({ endpoints })
 })
 
-apiRouter.get("/users", getUsers)
+// apiRouter.get("/users", getUsers)
+apiRouter.use("/users", usersRouter)
 apiRouter.get("/plants", getPlants)
 apiRouter.get("/found_plants", getFoundPlants)
 
