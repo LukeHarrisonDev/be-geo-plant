bc2eb2ab23046718e063f5cca86e2cae9e2819de
BassManLuke <luke_haz@yahoo.co.uk>
2024-09-27 16:21:19 +0100

    add error handling if the user_id doesn't exist

--- models/users.models.js
+++ models/users.models.js
@@ -9,11 +9,6 @@ function fetchUsers() {
 }
 
 function fetchUserById(userId) {
-    // console.log(+userId, "<<< User Id")
-    // console.log(typeof +userId, "<<< Type")
-    // if (typeof +userId !== "number") {
-    //     return Promise.reject({ status: 404, message: "Not Found" })
-    // }
     let sqlQuery = `SELECT users.*, COUNT(found_plants.found_by) AS plants_count
     FROM users
     LEFT JOIN found_plants
@@ -22,6 +17,9 @@ function fetchUserById(userId) {
     GROUP BY users.user_id`
     return db.query(sqlQuery, [userId])
     .then(({ rows }) => {
+        if (rows.length === 0) {
+            return Promise.reject({ status: 404, message: "Not Found" })
+        }
         return rows[0]
     })
 }
