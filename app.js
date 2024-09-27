c4a8b918dcea92e0fedc51612af629b89e72ad7e
BassManLuke <luke_haz@yahoo.co.uk>
2024-09-27 11:34:00 +0100

    add apirouter

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require("express")
+const apiRouter = require("./routes/api-router")
 const { getUsers } = require("./controllers/users.controllers")
 const { getPlants } = require("./controllers/plants.controllers")
 const { getFoundPlants } = require("./controllers/found-plants.controller")
@@ -6,14 +7,12 @@ const { catchInvalidEndpoints } = require("./error-handling")
 const app = express()
 const endpoints = require("./endpoints.json")
 
-app.get("/api", (request, response) => {
-    response.status(200).send({ endpoints })
-})
+app.use("/api", apiRouter)
 
 app.get("/api/users", getUsers)
 app.get("/api/plants", getPlants)
 app.get("/api/found_plants", getFoundPlants)
 
-app.all("/*", catchInvalidEndpoints);
+app.all("/*", catchInvalidEndpoints)
 
 module.exports = app
