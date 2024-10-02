b85c1efa24d24f93dcf3b3609a3401a9780412f6
BassManLuke <luke_haz@yahoo.co.uk>
2024-10-02 15:43:04 +0100

    add postFoundPlant with only the required fields

--- controllers/found-plants.controllers.js
+++ controllers/found-plants.controllers.js
@@ -1,19 +1,8 @@
-const { fetchAllFoundPlants, fetchFoundPlantById, fetchFoundPlantsByUserId, } = require("../models/found-plants.models")
+const { fetchAllFoundPlants, fetchFoundPlantById, fetchFoundPlantsByUserId, addFoundPlant, } = require("../models/found-plants.models")
 
 function getAllFoundPlants(request, response, next) {
     fetchAllFoundPlants()
     .then((foundPlants) => {
-        response.status(200).send({ foundPlants })
-    })
-    .catch((error) => {
-        next(error)
-    })
-}
-
-function getFoundPlantsByUserId(request, response, next) {
-    const user_id = request.params.user_id
-    fetchFoundPlantsByUserId(user_id)
-    .then(( foundPlants ) => {
         response.status(200).send({ foundPlants })
     })
     .catch((error) => {
@@ -32,4 +21,24 @@ function getFoundPlantById(request, response, next) {
     })
 }
 
-module.exports = { getAllFoundPlants, getFoundPlantsByUserId, getFoundPlantById }
+function getFoundPlantsByUserId(request, response, next) {
+    const user_id = request.params.user_id
+    fetchFoundPlantsByUserId(user_id)
+    .then(( foundPlants ) => {
+        response.status(200).send({ foundPlants })
+    })
+    .catch((error) => {
+        next(error)
+    })
+}
+
+function postFoundPlant(request, response, next) {
+    const user_id = request.params.user_id
+    const newFoundPlant = request.body
+    addFoundPlant(user_id, newFoundPlant)
+    .then((foundPlant) => {
+        response.status(201).send({ foundPlant })
+    })
+}
+
+module.exports = { getAllFoundPlants, getFoundPlantsByUserId, getFoundPlantById, postFoundPlant }
--- routes/users-router.js
+++ routes/users-router.js
@@ -1,4 +1,4 @@
-const { getFoundPlantsByUserId } = require("../controllers/found-plants.controllers")
+const { getFoundPlantsByUserId, postFoundPlant } = require("../controllers/found-plants.controllers")
 const { getUsers, getUserById, postUser, deleteUserById } = require("../controllers/users.controllers")
 
 const usersRouter = require("express").Router()
@@ -10,5 +10,6 @@ usersRouter.get("/:user_id", getUserById)
 usersRouter.delete("/:user_id", deleteUserById)
 
 usersRouter.get("/:user_id/found_plants", getFoundPlantsByUserId)
+usersRouter.post("/:user_id/found_plants", postFoundPlant)
 
 module.exports = usersRouter
