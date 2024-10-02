f146d84ee4f7534f081490fa94f5f788a708f688
BassManLuke <luke_haz@yahoo.co.uk>
2024-10-02 14:16:49 +0100

    update getFoundPlants functions to getAllFoundPlants

--- routes/found-plants-router.js
+++ routes/found-plants-router.js
@@ -1,8 +1,8 @@
-const { getFoundPlants, getFoundPlantById } = require("../controllers/found-plants.controllers")
+const { getAllFoundPlants, getFoundPlantById } = require("../controllers/found-plants.controllers")
 
 const foundPlantsRouter = require("express").Router()
 
-foundPlantsRouter.get("/", getFoundPlants)
+foundPlantsRouter.get("/", getAllFoundPlants)
 foundPlantsRouter.get("/:find_id", getFoundPlantById)
 
 module.exports = foundPlantsRouter
