80ebe036b123828e83943b06a1ba82981731c5e9
BassManLuke <luke_haz@yahoo.co.uk>
2025-01-10 22:05:45 +0000

    add error handling for seasons filter

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -697,37 +697,6 @@ describe("/api/users/:user_id/found_plants", () => {
                     })
                 })
             })
-            test("?season=&order_by=&sort_by= 200: Responds with the given users found plants with multiple queries", () => {
-                const seasons = ["Winter", "Spring"]
-                return request(app)
-                .get("/api/users/3/found_plants?season=Winter&season=Spring&order_by=asc&sort_by=location_name")
-                .set("lat", "53.79354")
-                .set("lon", "-1.75064")
-                .expect(200)
-                .then(({body}) => {
-                    expect(body.foundPlants).toHaveLength(5)
-                    expect(body.foundPlants).toBeSortedBy("location_name", { descending: false, coerce: true })
-                    body.foundPlants.forEach((foundPlant) => {
-                        expect(
-                            seasons.some((season) => foundPlant.season.includes(season))
-                        ).toBe(true)
-                        expect(foundPlant).toMatchObject({
-                            find_id: expect.any(Number),
-                            plant_id: expect.any(Number),
-                            plant_name: expect.any(String),
-                            found_by: 3,
-                            photo_url: expect.any(String),
-                            location_name: expect.any(String),
-                            comment: expect.any(String),
-                            created_at: expect.any(String),
-                            location: expect.objectContaining({
-                                lat: expect.any(Number),
-                                lon: expect.any(Number),
-                            })
-                        })
-                    })
-                })
-            })
             test("?season= 200: Responds with the given users found plants that are typically found in a single given season", () => {
                 return request(app)
                 .get("/api/users/3/found_plants?season=Spring")
@@ -759,6 +728,37 @@ describe("/api/users/:user_id/found_plants", () => {
                 .expect(400)
                 .then(({body}) => {
                     expect(body).toEqual({message: "Bad Request"})
+                })
+            })
+            test("?season=&order_by=&sort_by= 200: Responds with the given users found plants with multiple queries", () => {
+                const seasons = ["Winter", "Spring"]
+                return request(app)
+                .get("/api/users/3/found_plants?season=Winter&season=Spring&order_by=asc&sort_by=location_name")
+                .set("lat", "53.79354")
+                .set("lon", "-1.75064")
+                .expect(200)
+                .then(({body}) => {
+                    expect(body.foundPlants).toHaveLength(5)
+                    expect(body.foundPlants).toBeSortedBy("location_name", { descending: false, coerce: true })
+                    body.foundPlants.forEach((foundPlant) => {
+                        expect(
+                            seasons.some((season) => foundPlant.season.includes(season))
+                        ).toBe(true)
+                        expect(foundPlant).toMatchObject({
+                            find_id: expect.any(Number),
+                            plant_id: expect.any(Number),
+                            plant_name: expect.any(String),
+                            found_by: 3,
+                            photo_url: expect.any(String),
+                            location_name: expect.any(String),
+                            comment: expect.any(String),
+                            created_at: expect.any(String),
+                            location: expect.objectContaining({
+                                lat: expect.any(Number),
+                                lon: expect.any(Number),
+                            })
+                        })
+                    })
                 })
             })
         })
@@ -998,6 +998,14 @@ describe("/api/users/:user_id/plants", () => {
                             find_amount: expect.any(Number),
                         })
                     })
+                })
+            })
+            test("?season= 400: Responds with 'Bad Request' when the given season is invalid", () => {
+                return request(app)
+                .get("/api/users/3/plants?season=not-a-season")
+                .expect(400)
+                .then(({body}) => {
+                    expect(body).toEqual({message: "Bad Request"})
                 })
             })
         })
