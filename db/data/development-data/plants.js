7516baf1ce038cd1e3d13cd92931240ac4355352
BassManLuke <luke_haz@yahoo.co.uk>
2025-01-15 14:13:34 +0000

    undo changes

--- db/data/development-data/plants.js
+++ db/data/development-data/plants.js
@@ -2,140 +2,140 @@ module.exports = [
     {
         plant_name: "Bluebell",
         about_plant: "Bluebells are native to the UK, commonly found in woodlands and hedgerows. They bloom in spring and have a sweet fragrance.",
-        plant_image_url: "https://media.istockphoto.com/id/147913339/photo/bluebells.jpg?s=2048x2048&w=is&k=20&c=IonMIAIHpJP8y0IhLQZfQDdy9hfFjAM9mNtio-T_egQ=",
+        plant_image_url: "https://images.unsplash.com/photo-1561938299-e3a547a5c234?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
         rarity: 50,
         season: ["Spring"]
     },
     {
         plant_name: "Common Bracken",
         about_plant: "Bracken is a perennial plant that grows in woodlands, heaths, and hills. It is one of the most abundant ferns in the UK.",
-        plant_image_url: "https://media.istockphoto.com/id/975061808/photo/fern-leaf-in-the-woods-common-bracken-summertime.jpg?s=2048x2048&w=is&k=20&c=JZ3NlfzyDuRQQEqva2_BnKYZD5JM2QB1hXY2rzIWz00=",
+        plant_image_url: "https://images.unsplash.com/photo-1598910018751-b8a83d4cccf2?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
         rarity: 20,
         season: ["Summer", "Autumn"]
     },
     {
         plant_name: "Dandelion",
         about_plant: "Dandelions are common in lawns, meadows, and roadsides. They are known for their bright yellow flowers and medicinal uses.",
-        plant_image_url: "https://media.istockphoto.com/id/157508183/photo/yellow-dandelions-and-green-grass.jpg?s=2048x2048&w=is&k=20&c=ZvUWU76a5PKRgjoIx29Jyc3w6q8jQ93xo0Kckhp7rsU=",
+        plant_image_url: "https://images.unsplash.com/photo-1618877120264-1cf02c21c888?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
         rarity: 10,
         season: ["Spring", "Summer"]
     },
     {
         plant_name: "Stinging Nettle",
         about_plant: "Stinging nettles are commonly found in damp soils and are known for their painful sting. They are rich in vitamins and have medicinal properties.",
-        plant_image_url: "https://media.istockphoto.com/id/1344803655/photo/common-stinging-nettle.jpg?s=2048x2048&w=is&k=20&c=seVq_5uqeLrbbynpGfG-c4Peudm5n3ySiCgKUBnHIxE=",
+        plant_image_url: "https://images.unsplash.com/photo-1579928454978-d3b795d9b9ad?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
         rarity: 30,
         season: ["Spring", "Summer"]
     },
     {
         plant_name: "Common Daisy",
         about_plant: "The common daisy is often seen in lawns and grasslands. It has a white and yellow flower and is associated with spring and summer.",
-        plant_image_url: "https://media.istockphoto.com/id/1216195118/photo/white-common-daisy-bellis-perennis-extreme-close-up.jpg?s=2048x2048&w=is&k=20&c=sqq8EPyN9MYdpG_PA3dq1ZB9StGwEIhO5cTrnh_TGaU=",
+        plant_image_url: "https://images.unsplash.com/photo-1608209522219-22c27d98e575?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
         rarity: 40,
         season: ["Spring", "Summer"]
     },
     {
         plant_name: "Wild Garlic",
         about_plant: "Wild garlic, also known as ramsons, is often found in woodlands. It has a strong garlic scent and is used in cooking.",
-        plant_image_url: "https://media.istockphoto.com/id/1222858995/photo/blooming-wild-garlic.jpg?s=2048x2048&w=is&k=20&c=Qn7K8lU1q8Ff5rR1RZwBvZfwfXagYm_X-my82FUCKU0=",
+        plant_image_url: "https://images.unsplash.com/photo-1600174916429-2b9ba28ab9c6?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
         rarity: 60,
         season: ["Spring"]
     },
     {
         plant_name: "Common Thistle",
         about_plant: "Common thistles grow in meadows and wastelands. Their prickly leaves protect the plant, and their purple flowers are distinctive.",
-        plant_image_url: "https://media.istockphoto.com/id/2172379275/photo/closeup-of-bull-thistle-bud-with-green-blurred-plants-on-background.jpg?s=2048x2048&w=is&k=20&c=tRpQPOctbAvJQbmTqr_UWcGt3z256bOL08l1gK3PoOM=",
+        plant_image_url: "https://images.unsplash.com/photo-1612208902750-2f27edba0893?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
         rarity: 25,
         season: ["Summer"]
     },
     {
         plant_name: "Lady's Mantle",
         about_plant: "Lady's mantle is a herbaceous perennial with rounded, scalloped leaves and small yellow-green flowers. It grows in moist woodlands.",
-        plant_image_url: "https://media.istockphoto.com/id/809037498/photo/closeup-of-mantle-flowers-in-water-drops-after-rain-ladys-mantle-perennial-garden-ornamental.jpg?s=2048x2048&w=is&k=20&c=3YOi2jAZYkQ-18-RQpsExZJkzKEsf217Yi8b_mjGOMc=",
+        plant_image_url: "https://images.unsplash.com/photo-1605660283394-3198da48f245?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
         rarity: 40,
         season: ["Spring", "Summer"]
     },
     {
         plant_name: "Primrose",
         about_plant: "Primroses are small, brightly colored flowers that bloom in early spring. They thrive in woodlands and meadows.",
-        plant_image_url: "https://media.istockphoto.com/id/839421978/photo/primrose-in-woodland.jpg?s=2048x2048&w=is&k=20&c=lIgR4zw4ukTwd5xBestU21ernj0ARAU6nhwfSc3Yot8=",
+        plant_image_url: "https://images.unsplash.com/photo-1611049337798-35b933ed84a9?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
         rarity: 35,
         season: ["Spring"]
     },
     {
         plant_name: "Cuckoo Flower",
         about_plant: "The cuckoo flower is a native wildflower that blooms in spring. It is also known as the cardamine pratensis and grows in meadows.",
-        plant_image_url: "https://media.istockphoto.com/id/183826962/photo/pale-lilac-cuckoo-flower-cardamine-pratensis-waterside-wildflower.jpg?s=2048x2048&w=is&k=20&c=zgRPwjROyMsrzNhwjEHob30BRdT98ttvwIzpevK5EuQ=",
+        plant_image_url: "https://images.unsplash.com/photo-1561674733-b29ffbfc7f5b?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
         rarity: 30,
         season: ["Spring"]
     },
     {
         plant_name: "Wild Marjoram",
         about_plant: "Wild marjoram is a perennial herb found in dry, sunny locations. It has aromatic leaves and pink or purple flowers.",
-        plant_image_url: "https://media.istockphoto.com/id/1363312812/photo/pink-flower-background-picture.jpg?s=2048x2048&w=is&k=20&c=voo41Y9UDGhXKj3E-QCL3YdB1I-qTVR54GcbZ4yMk2U=",
+        plant_image_url: "https://images.unsplash.com/photo-1554464084-8f79b6b3d102?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
         rarity: 20,
         season: ["Summer"]
     },
     {
         plant_name: "Yarrow",
         about_plant: "Yarrow is a hardy herb with white, yellow, or pink flowers that bloom in summer. It is commonly found in grasslands and open fields.",
-        plant_image_url: "https://media.istockphoto.com/id/2162148932/photo/yellow-yarrow-or-achillea-millefolium-blooming-in-summer.jpg?s=2048x2048&w=is&k=20&c=dolmr2U1GGtdphD6Bb2GIg0fw6ENjqug2XJP4iufOO4=",
+        plant_image_url: "https://images.unsplash.com/photo-1532102228359-bd501ad5cdd9?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
         rarity: 15,
         season: ["Summer"]
     },
     {
         plant_name: "Cow Parsley",
         about_plant: "Cow parsley is a common plant in hedgerows and roadside verges. It has delicate white flowers and fern-like leaves.",
-        plant_image_url: "https://media.istockphoto.com/id/1451124918/photo/cow-parsley.jpg?s=2048x2048&w=is&k=20&c=smMVHlWc6sZZRLOxAHYHLsOCnEhy9OnwejXNWUYgDMA=",
+        plant_image_url: "https://images.unsplash.com/photo-1592910783472-43a81b161d56?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
         rarity: 40,
         season: ["Spring", "Summer"]
     },
     {
         plant_name: "Hogweed",
         about_plant: "Hogweed is a large plant found in wetlands and along roadsides. It has large white flower clusters and is often considered invasive.",
-        plant_image_url: "https://media.istockphoto.com/id/1394277879/photo/hogweed-plant-background.jpg?s=2048x2048&w=is&k=20&c=KA1Nw0NbAAPJKUPbwrAVLhSyQ8Do2RmzDYx0rK0L0t0=",
+        plant_image_url: "https://images.unsplash.com/photo-1587119435634-0f233d2b16fe?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
         rarity: 10,
         season: ["Summer", "Autumn"]
     },
     {
         plant_name: "Common Poppy",
         about_plant: "Common poppies are iconic red flowers that bloom in summer and are often found in fields and meadows.",
-        plant_image_url: "https://media.istockphoto.com/id/1405017562/photo/red-corn-poppies-in-a-summer-meadow.jpg?s=2048x2048&w=is&k=20&c=YuSRAG5po4BwDOmDFfQr6Ulyz00ceaQaYWcYsVUliZg=",
+        plant_image_url: "https://images.unsplash.com/photo-1565290184-b21cfd38780f?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
         rarity: 50,
         season: ["Summer"]
     },
     {
         plant_name: "Blackberry",
         about_plant: "Blackberry bushes are commonly found in hedgerows and produce dark purple-black fruit in late summer. The leaves have medicinal uses.",
-        plant_image_url: "https://media.istockphoto.com/id/1280935288/photo/ripe-shiny-blackberries-on-a-bush-juicy-sweet-berry-on-a-branch.jpg?s=2048x2048&w=is&k=20&c=63Mh_NDipTvxLMZlussS-76pRBuHfNbvfcvK5NxWhKk=",
+        plant_image_url: "https://images.unsplash.com/photo-1596031065545-9cc93a8099db?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
         rarity: 25,
         season: ["Summer", "Autumn"]
     },
     {
         plant_name: "Silver Birch",
         about_plant: "Silver birch is a deciduous tree with white bark that grows in woodlands and open fields. Its catkins are a characteristic feature in early spring.",
-        plant_image_url: "https://media.istockphoto.com/id/1345661749/photo/betula-pendula-roth-tree.jpg?s=2048x2048&w=is&k=20&c=xPbIPqeMopqlyRyp0BkmZ7-OpGXrwmEDrPQsVX64_Gc=",
+        plant_image_url: "https://images.unsplash.com/photo-1569435479-1b76f85e6f63?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
         rarity: 30,
         season: ["Spring"]
     },
     {
         plant_name: "Red Campion",
         about_plant: "Red campion is a wildflower that blooms in spring and summer. Its pink or purple flowers are commonly found in woodlands and meadows.",
-        plant_image_url: "https://media.istockphoto.com/id/186652244/photo/red-campion.jpg?s=2048x2048&w=is&k=20&c=15zarUnWW8_bEB4gtkV8arcT2I5ViHgY0K3lMf90lHY=",
+        plant_image_url: "https://images.unsplash.com/photo-1561591750-e5e26c080767?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
         rarity: 20,
         season: ["Spring", "Summer"]
     },
     {
         plant_name: "Hedgerow Cranesbill",
         about_plant: "Hedgerow cranesbill is a member of the geranium family with purple flowers that bloom from late spring to early summer in hedgerows and grasslands.",
-        plant_image_url: "https://media.istockphoto.com/id/2158164470/photo/purple-and-white-geranium-pyrenaicum-hedgerow-cranesbill-in-flower.jpg?s=2048x2048&w=is&k=20&c=EzQc5U5jG-3PDDda0hhJ5Xp3h5cHNOm6zQlBJVvoNAs=",
+        plant_image_url: "https://images.unsplash.com/photo-1565027546-7d804a9837b8?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
         rarity: 10,
         season: ["Spring", "Summer"]
     },
     {
         plant_name: "Hawthorn",
         about_plant: "Hawthorn is a shrub or small tree found in hedgerows and woodlands. It blooms with white or pink flowers in late spring and early summer.",
-        plant_image_url: "https://media.istockphoto.com/id/498243317/photo/great-tit.jpg?s=2048x2048&w=is&k=20&c=NpXEng-P_bAM7bIRIZ2Jg8ZWhtq9hjlmUmO5h97Kp64=",
+        plant_image_url: "https://images.unsplash.com/photo-1584760849563-d63c1e5c52ab?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
         rarity: 30,
         season: ["Spring"]
     }
