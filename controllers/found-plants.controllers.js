c79f69ab1bb44c8706ed42983331dce414aef0d6
BassManLuke <luke_haz@yahoo.co.uk>
2025-01-24 15:55:35 +0000

    remove exiftool from controller

--- controllers/found-plants.controllers.js
+++ controllers/found-plants.controllers.js
@@ -1,5 +1,3 @@
-const exiftool = require("exiftool-vendored").exiftool
-
 const { fetchAllFoundPlants, fetchFoundPlantById, fetchFoundPlantsByUserId, addFoundPlant, removeFoundPlantById, } = require("../models/found-plants.models")
 
 function getAllFoundPlants(request, response, next) {
