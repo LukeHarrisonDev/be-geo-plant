85134f7ed67ffd37b388e9df5c22baaac28e2a50
BassManLuke <luke_haz@yahoo.co.uk>
2025-01-31 22:47:04 +0000

    create scripts and complete authentication seeding

--- db/seeds/run-auth-seed-test.js
+++ db/seeds/run-auth-seed-test.js
@@ -1,11 +1,10 @@
-const devData = require('../data/development-data/index.js');
 const testData = require("../data/test-data/index.js")
 const authSeed = require('./auth-seed.js');
 const db = require('../connection.js');
 
 const runAuthSeed = () => {
   return authSeed(testData)
-//   .then(() => db.end());
+  // .then(() => db.end());
 };
 
 runAuthSeed();
