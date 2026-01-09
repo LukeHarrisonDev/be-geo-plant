40a67b4b5c0db34bab7ed261909cdab5aee3ea1d
BassManLuke <luke_haz@yahoo.co.uk>
2026-01-09 12:00:03 +0000

    update connections.js

--- db/connection.js
+++ db/connection.js
@@ -11,8 +11,7 @@ if (!process.env.PGDATABASE && !process.env.DATABASE_URL) {
 
 const config = {};
 
-// if (ENV === 'production') {
-if (process.env.DATABASE_URL) {
+if (ENV === 'production') {
   config.connectionString = process.env.DATABASE_URL;
   config.max = 2;
 }
@@ -20,12 +19,7 @@ if (process.env.DATABASE_URL) {
 console.log("NODE_ENV:", process.env.NODE_ENV);
 console.log("ENV:", ENV);
 console.log("Has DATABASE_URL:", !!process.env.DATABASE_URL);
-console.log(
-  "DB host from DATABASE_URL:",
-  process.env.DATABASE_URL
-    ? new URL(process.env.DATABASE_URL).hostname
-    : "none"
-);
+console.log("DB host from DATABASE_URL:", process.env.DATABASE_URL ? new URL(process.env.DATABASE_URL).hostname : "none" );
 console.log("pg config keys:", Object.keys(config));
 
 module.exports = new Pool(config);
