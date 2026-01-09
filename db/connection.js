41c9f547f05350793a4ec1168fa30af7cfede222
BassManLuke <luke_haz@yahoo.co.uk>
2026-01-09 11:39:00 +0000

    change connection and add console logs

--- db/connection.js
+++ db/connection.js
@@ -11,9 +11,21 @@ if (!process.env.PGDATABASE && !process.env.DATABASE_URL) {
 
 const config = {};
 
-if (ENV === 'production') {
+// if (ENV === 'production') {
+if (process.env.DATABASE_URL) {
   config.connectionString = process.env.DATABASE_URL;
   config.max = 2;
 }
+
+console.log("NODE_ENV:", process.env.NODE_ENV);
+console.log("ENV:", ENV);
+console.log("Has DATABASE_URL:", !!process.env.DATABASE_URL);
+console.log(
+  "DB host from DATABASE_URL:",
+  process.env.DATABASE_URL
+    ? new URL(process.env.DATABASE_URL).hostname
+    : "none"
+);
+console.log("pg config keys:", Object.keys(config));
 
 module.exports = new Pool(config);
